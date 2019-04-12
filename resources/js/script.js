6c6323e77eefc39dc4dbcf2741873aa73e2f3eb1
Myron Walters <myron.walters01@gmail.com>
2019-04-12 13:28:19 -0400

    Added Jquery scrolling animation
    Added smooth scrolling animation jquery to all links
    
    Figured out how to offset scroll distance

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
     
-   $('h1').click(function() {
-       $(this).css('background-color', '#ff0000')
-   }) 
+    
+   /* Scroll on buttons */
+    $('.js--scroll-to-form').click(function () {
+       $('html, body').animate({scrollTop: $('.js--form').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-partners').click(function () {
+       $('html, body').animate({scrollTop: $('.js--partners').offset().top-40}, 1000); 
+    });
+    
+    
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top-70
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
     
 });
