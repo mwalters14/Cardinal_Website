1a466d8a9f2bd1a278df5578fd08f9635176e5c3
Myron Walters <myron.walters01@gmail.com>
2019-05-09 21:29:44 -0400

    Spring file structure

--- src/main/java/siteHandler/CustomerRepository.java
+++ src/main/java/siteHandler/CustomerRepository.java
@@ -3,5 +3,12 @@ package siteHandler;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/* Learning comments
+    A class can extend only one abstract class
+    A class can implement more than one interface
+
+    interface - An interface is a special form of an abstract class which does not implement any methods
+    extends - CustomerRepository inherits the attributes and methods of the JpaRepository
+*/
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> { }
